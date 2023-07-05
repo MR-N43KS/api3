1301d59ba36579d0bddec615b4d12d5a013fa2a0
akoofice <129665695+akoofice@users.noreply.github.com>
2023-07-05 13:18:10 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ global.creator = 'AkoOffice'// yourname
 global.MONGO_DB_URI = "mongodb+srv://akojelish:293848afn@cluster0.brucfuu.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "akoyourname82@gmail.com" //email
-global.email_password = "ltkmcxcdwrdkiqoj" //application password email
+global.email_password = "gzwe edfv pnsg avad" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
